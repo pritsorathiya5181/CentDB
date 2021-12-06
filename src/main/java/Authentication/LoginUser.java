786fb88f98d0aa8762abbe721f1a527f24984fe6
Krutarth Patel <kr653484@dal.ca>
2021-12-06 11:05:46 -0400

    Update LoginUser.java

--- src/main/java/Authentication/LoginUser.java
+++ src/main/java/Authentication/LoginUser.java
@@ -8,7 +8,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class LoginUser {
-    public UserModel LoginUser;
+    public UserModel LoginUser = new UserModel();
     public boolean login() throws FileNotFoundException, NoSuchAlgorithmException {
         Scanner sc = new Scanner(System.in);
 
@@ -18,17 +18,37 @@ public class LoginUser {
         System.out.println("Enter password");
         String password = sc.next();
 
+
+
         File myFile = new File(fileLocation.USER_CREDENTIAL_PATH);
         Scanner fileReader = new Scanner(myFile);
         while (fileReader.hasNextLine()){
             String data = fileReader.nextLine();
-            if(username.equals(data.split(";")[0])) {
+            String [] userMeta = data.split(";");
+
+            if(username.equals(userMeta[0])) {
+
+                System.out.println("Answer your security question");
+                System.out.println(userMeta[2]);
+                String givenAnswer = sc.next();
                 final String hashedPassword = HashAlgorithm.toHexString(HashAlgorithm.getSHA(password));
-                return hashedPassword.equals(data.split(";")[1]);
+                final String rightAnswer = userMeta[3];
+
+                if(hashedPassword.equals(userMeta[4]) && givenAnswer.equals(rightAnswer)){
+                    LoginUser.setUsername(userMeta[0]);
+                    LoginUser.setEmail(userMeta[1]);
+                    LoginUser.setSecurityQuestion(userMeta[2]);
+                    LoginUser.setSecurityAnswer(userMeta[3]);
+                    LoginUser.setPassword(userMeta[4]);
+
+                    return true;
+                }
+
             }
         }
-        UserModel.getinstance().setUsername(username);
-        UserModel.getinstance().setPassword(password);
+
+//        this.LoginUser = new UserModel(username,password,"email","dummy security","dummy answer");
+
         return false;
     }
 }
