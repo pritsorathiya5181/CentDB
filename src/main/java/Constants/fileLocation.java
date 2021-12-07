de50db0d543d6f796e78983d7d345a5437d22579
Krutarth Patel <krutarthzero@hotmail.com>
2021-12-07 02:51:43 -0400

    analytics done

--- src/main/java/Constants/fileLocation.java
+++ src/main/java/Constants/fileLocation.java
@@ -3,4 +3,5 @@ package Constants;
 public class fileLocation {
     public static final String LOCAL_PATH = System.getProperty("user.dir") + "/assets";
     public static final String USER_CREDENTIAL_PATH = System.getProperty("user.dir")+"/src/main/java/authentication/userCredential.txt";
+    public static final String ANALYTICS_PATH = System.getProperty("user.dir") + "/analytics";
 }
